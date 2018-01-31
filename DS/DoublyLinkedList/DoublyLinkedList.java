c780007eaa099d769dd678885144bff0c3d9ad35
Kumar <kumar.gaurav.ciit@gmail.com>
2018-01-31 12:35:58 +0530

    programs for DoublyLinkedList operations

---
+++ DS/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,107 @@
+public class DoublyLinkedList {
+
+	Node head;
+
+	static class Node{
+		
+		int data;
+		Node next, previous;
+		
+		public Node(int data){
+			this.data = data;
+			next = previous = null;
+		}
+	
+	}
+	
+	public Node getNewNode(int x){
+		return new Node(x);
+	}
+	
+	public void insertAtHead(int x){
+		Node newNode = getNewNode(x);
+		if(head == null){
+			head = newNode;
+			return;
+		}
+		head.previous = newNode;
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public void insertAtTail(int x){
+		Node newNode = getNewNode(x);
+		Node temp = head;
+		if(head == null){
+			head = newNode;
+			return;
+		}
+		while(temp.next != null){
+			temp = temp.next;
+		}
+		temp.next = newNode;
+		newNode.previous = temp;
+	}
+	
+	public void print(){
+		Node temp = head;
+		System.out.print("Forward: ");
+		while(temp != null){
+			System.out.print(" " + temp.data);
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public void sortedInsert(int data) {
+		Node newNode = getNewNode(data);
+		Node temp = head;
+    
+		if(head == null || head.data >= data){
+			insertAtHead(data);
+		}
+    
+		boolean inserted = false;
+		
+		while(temp.next != null){
+			if(temp.next.data >= data){
+				temp.next.previous = newNode;
+				newNode.next = temp.next;
+				temp.next = newNode;
+				inserted = true;
+				break;
+			}
+			temp = temp.next;
+		}
+		if(!inserted && temp != null){
+			newNode.previous = temp;
+			temp.next = newNode;
+		}
+		
+	}
+	
+	public void reversePrint(){
+		Node temp = head;
+		if(temp == null) return; // empty list, exit
+		// Going to last Node
+		while(temp.next != null) {
+			temp = temp.next;
+		}
+		System.out.print("Reverse: ");
+		while(temp != null) {
+			System.out.print(" " + temp.data);
+			temp = temp.previous;
+		}
+		System.out.print("\n");
+	}
+	
+	public static void main(String[] args){
+		
+		DoublyLinkedList DL = new DoublyLinkedList();
+		DL.insertAtHead(6); DL.print(); DL.reversePrint();
+		DL.insertAtHead(5); DL.print(); DL.reversePrint();
+		DL.insertAtHead(2); DL.print(); DL.reversePrint();
+		DL.sortedInsert(3); DL.print(); DL.reversePrint();
+		
+	}
+}
